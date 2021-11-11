00e6a9cb47b86d7a99e524d865c7486fe72d4ffb
AberRose <abeer88.a@gmail.com>
2021-11-11 10:14:33 +0100

    Template anpassen

--- vergleichNeu/flaskVergleich/vergleich.py
+++ vergleichNeu/flaskVergleich/vergleich.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 
 def home_page():
-   return render_template('home.html')
+    return render_template('home.html', pagetitle='abeer')
 
 
 
+if __name__ == "__main__":
+   app.run()
