f4b4d82dba5faf86437825ceaae6375afc987cd9
AberRose <abeer88.a@gmail.com>
2021-11-19 18:19:07 +0100

    add Dic , template anpassen

--- vergleichNeu/backend/suchfunktion.py
+++ vergleichNeu/backend/suchfunktion.py
@@ -1,21 +1,66 @@
-Staedte={
-    "Hamburg" :1000,
-    "Berlin":3000,
-    "Lübeck":4000,
-    "Neumünster":5000,
-    }
-#TODO dict in parameter
-list = []
 
-def main_search(query,query2):
-   list =[]
-   for key, value in Staedte.items():
-       if query  in key:
-           list.append(query)
-           list.append(value)
-       if  query2  in key:
-           list.append(query2)
-           list.append(value)
-           print(list)
-   return(list)
+
+import requests, json
+
+def get_incidence(city_id):
+    url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?"
+    parameter = {
+        'referer':'https://www.mywebapp.com',
+        'user-agent':'python-requests/2.9.1',
+        'where': f'AdmUnitId = {city_id}', # Welche landkreise sollen zurück gegeben werden
+        'outFields': '*', # Rückgabe aller Felder
+        'returnGeometry': False, # Keine Geometrien
+        'f':'json', # Rückgabeformat, hier JSON
+        'cacheHint': True # Zugriff über CDN anfragen
+    }
+    result = requests.get(url=url, params=parameter) #Anfrage absetzen
+    resultjson = json.loads(result.text) # Das Ergebnis JSON als Python Dictionary laden
+    incidence = resultjson['features'][0]['attributes']['Inz7T']
+    return incidence
+
+# Funktion ruft Inzidenzen für 10 gr Städte auf und speichert sie in dictionary
+
+def getBigCities():
+    berlin = get_incidence(11)
+    munich = get_incidence(9162)
+    hamburg= get_incidence(2000)
+    cologne = get_incidence(5315)
+    frankfurt = get_incidence(6412)
+    stuttgart = get_incidence(8111)
+    duesseldorf = get_incidence(5111)
+    dortmund = get_incidence(5913)
+    essen = get_incidence(5113)
+
+    dict={}
+    dict['Berlin'] =berlin
+    dict['München'] = munich
+    dict['Hamburg'] = hamburg
+    dict['Köln'] = cologne
+    dict['Frankfurt'] = frankfurt
+    dict['Stuttgart'] = stuttgart
+    dict['Düsseldorf'] = duesseldorf
+    dict['Dortmund'] = dortmund
+    dict['Essen'] = essen
+
+    return dict
+
+city_dict = getBigCities()
+
+def city_search(city1, city2):
+    list = []
+
+    for key, value in city_dict.items():
+        if city1 in key:
+            list.append(city1)
+            list.append(value)
+
+        if city2 in key:
+            list.append(city2)
+            list.append(value)
+
+    return (list)
+
+
+
+
 
--- vergleichNeu/vergleich.py
+++ vergleichNeu/vergleich.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request
 
 import sys
 import time
-
-from backend.suchfunktion import main_search
-
+from backend.suchfunktion import city_search
 sys.path.append('/C/Users/abeer/PycharmProjects/pythonProject1/Inzidenzvergleich/vergleichNeu/backend')
 
 app = Flask(__name__)
@@ -20,13 +18,13 @@ def home_page():
     # suchwort2 = spacing
     if request.method == 'POST':
         # hier suche nach eienr wort
-        start = time.time()
         suchwort = str(formData.get('input1'))
         suchwort2 = str(formData.get('input2'))
-        results = main_search(suchwort, suchwort2)
-        ende =  time.time()
-        seconds =ende-start
-        return render_template('home.html', len=len(results),seconds=seconds, results=results ,suchwort=suchwort,suchwort2=suchwort2 )
+        #start = time.time()
+        results = city_search(suchwort, suchwort2)
+        #ende =  time.time()
+        #seconds =ende-start
+        return render_template('home.html', len=len(results), results=results ,suchwort=suchwort,suchwort2=suchwort2 )
     else:
         return render_template('home.html')
 
