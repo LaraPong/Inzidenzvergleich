7804defc78a1341966bf27d7d928dfd0b1da066a
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2022-01-18 10:13:18 +0100

    Versucht UK im Frontend zu intergrieren

--- inzidenzvergleich.py
+++ inzidenzvergleich.py
@@ -4,19 +4,20 @@ import requests, json
 
 app = Flask(__name__)
 
+
 def get_incidence(city_id):
     url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?"
     parameter = {
-        'referer':'https://www.mywebapp.com',
-        'user-agent':'python-requests/2.9.1',
-        'where': f'AdmUnitId = {city_id}', # Welche landkreise sollen zurück gegeben werden
-        'outFields': '*', # Rückgabe aller Felder
-        'returnGeometry': False, # Keine Geometrien
-        'f':'json', # Rückgabeformat, hier JSON
-        'cacheHint': True # Zugriff über CDN anfragen
+        'referer': 'https://www.mywebapp.com',
+        'user-agent': 'python-requests/2.9.1',
+        'where': f'AdmUnitId = {city_id}',  # Welche landkreise sollen zurück gegeben werden
+        'outFields': '*',  # Rückgabe aller Felder
+        'returnGeometry': False,  # Keine Geometrien
+        'f': 'json',  # Rückgabeformat, hier JSON
+        'cacheHint': True  # Zugriff über CDN anfragen
     }
-    result = requests.get(url=url, params=parameter) #Anfrage absetzen
-    resultjson = json.loads(result.text) # Das Ergebnis JSON als Python Dictionary laden
+    result = requests.get(url=url, params=parameter)  # Anfrage absetzen
+    resultjson = json.loads(result.text)  # Das Ergebnis JSON als Python Dictionary laden
     incidence = resultjson['features'][0]['attributes']['Inz7T']
     return incidence
 
@@ -25,23 +26,25 @@ def get_incidence(city_id):
 
 def getBigCities():
     cities_incidences = {}
-    
-    big_cities = {# Das irgendwann direkt aus Datenbank oder Tabelle ablesen
-      'Berlin': 11,
-      'München': 9162,
-      'Hamburg': 2000,
-      'Köln': 5315,
-      'Frankfurt': 6412,
-      'Stuttgart': 8111,
-      'Düsseldorf': 5111,
-      'Dortmund': 5913,
-      'Essen': 5113 }
-    
+
+    big_cities = {  # Das irgendwann direkt aus Datenbank oder Tabelle ablesen
+        'Berlin': 11,
+        'München': 9162,
+        'Hamburg': 2000,
+        'Köln': 5315,
+        'Frankfurt': 6412,
+        'Stuttgart': 8111,
+        'Düsseldorf': 5111,
+        'Dortmund': 5913,
+        'Essen': 5113}
+
     for key, value in big_cities.items():
         cities_incidences[key] = get_incidence(value)
     return cities_incidences
 
+
 city_dict = getBigCities()
+
 
 def city_search(city1, city2):
     info = {}
@@ -56,9 +59,10 @@ def city_search(city1, city2):
 
 
 def tableGetDate():
-    #ToDo: Implement Database Call
-    
+    # ToDo: Implement Database Call
+
     return "1.1.2011"
+
 
 @app.route("/", methods=['GET', 'POST'])
 def homepage():
@@ -81,12 +85,14 @@ def homepage():
             error2 = 'Stadt 2 nicht gültig'
 
         if error1 or error2:
-            return render_template('homepage.html', error1=error1, error2=error2, suchwort=suchwort, suchwort2=suchwort2)
+            return render_template('homepage.html', error1=error1, error2=error2, suchwort=suchwort,
+                                   suchwort2=suchwort2)
 
         return render_template('homepage.html', results=results, suchwort=suchwort, suchwort2=suchwort2, xwerte=xwerte,
                                ywerte=ywerte)
     else:
         return render_template('homepage.html')
 
+
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
